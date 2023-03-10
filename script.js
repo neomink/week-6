4d4eaab2e9b0b9f582e37d3ef31e8b8372839a27
neomink <jungmin.kim.0711@gmail.com>
2023-03-10 00:57:32 -0500

    tlqkf!

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-console.log('Remove this console log and get started!')
+const stopMotion = {
+  stopMotion: [
+    {
+      title: 'MITOSIS',
+      videoIds: ["https://www.youtube.com/embed/araFF9plBHQ"]
+    },
+    {
+      title: 'FOIL',
+      videoIds: ["https://www.youtube.com/embed/tUPOCu6pj8M"]
+    },
+    {
+      title: 'HIDARI',
+      videoIds: ["https://www.youtube.com/embed/DpefYPLH67A"]
+    }
 
-const data = {
-  channels: [
-    // Add your channel data here
-    // {
-    //   title: 'UFOs',
-    //   videoIds: [
-    //     // Add your video IDs here
-    //   ]
-    // }
   ]
 }
+
+function showStopMotion() {
+  const showStopMotion = [stopMotion];
+  showStopMotion.forEach(function (showStopMotion) {
+    console.log(showStopMotion);
+  })
+}
+
